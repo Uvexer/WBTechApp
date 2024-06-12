34858a8d88ff1fab3287a0b01b47c7493d7d8d62
Uvexer <125573358+Uvexer@users.noreply.github.com>
2024-06-13 00:34:46 +0300

    Initial Commit

---
+++ WBTech/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WBTech
+//
+//  Created by bodya on 13.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WBTech/WBTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBTechApp.swift
+//  WBTech
+//
+//  Created by bodya on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
